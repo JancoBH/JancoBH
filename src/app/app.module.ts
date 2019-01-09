01e45a66da2ec947c4a6efd8e33bcc8b58a8f2fc
Janco <jancoboscan@gmail.com>
2019-01-09 18:00:15 -0300

    Mini cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,7 +46,7 @@ library.add(faTwitter, faLinkedin, faGithub);
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'JancoBH'),
-    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFirestoreModule.enablePersistence(), // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
     AngularFireStorageModule, // imports firebase/storage only needed for storage features
     AppRoutingModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyBU0uJrZy4OzbApRiTEhDU31xsXO_U79WU',
+    authDomain: 'jancobh-2.firebaseapp.com',
+    databaseURL: 'https://jancobh-2.firebaseio.com',
+    projectId: 'jancobh-2',
+    storageBucket: 'jancobh-2.appspot.com',
+    messagingSenderId: '591093196244'
   }
 };
